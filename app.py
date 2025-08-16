f62ddc4a0a08abcff4b7d3921b1e983c675fff06
Harish M <harishmurugan005arni@gmail.com>
2025-08-16 13:01:26 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+# Small list of motivational quotes
+quotes = [
+    "Believe in yourself, success will follow.",
+    "Hard work beats talent when talent doesn’t work hard.",
+    "Small steps every day lead to big achievements.",
+    "Focus on progress, not perfection.",
+    "Discipline is the bridge between goals and success."
+]
+
+@app.route("/")
+def home():
+    return "Welcome to Smart Quote Generator API! Use /quote to get a motivational quote."
+
+@app.route("/quote")
+def get_quote():
+    import random
+    return f"Motivational Quote: {random.choice(quotes)}"
+
+if __name__ == "__main__":
+    app.run(debug=True)
